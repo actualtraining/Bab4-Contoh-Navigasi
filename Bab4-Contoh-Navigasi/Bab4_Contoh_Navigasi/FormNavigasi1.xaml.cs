678eae776fbf0d063a4cd4c3ff061fdc993a2c59
erick <erick@DESKTOP-JIKBK0G>
2017-03-21 16:30:29 +0700

    Add project files.

---
+++ Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/FormNavigasi1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bab4_Contoh_Navigasi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormNavigasi1 : ContentPage
+    {
+        public FormNavigasi1()
+        {
+            InitializeComponent();
+            btnHal2.Clicked += BtnHal2_Clicked;
+        }
+
+        private async void BtnHal2_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FormNavigasi2());
+        }
+    }
+}
---
+++ Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/FormNavigasi2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bab4_Contoh_Navigasi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormNavigasi2 : ContentPage
+    {
+        public FormNavigasi2()
+        {
+            InitializeComponent();
+            btnForm1.Clicked += BtnForm1_Clicked;
+        }
+
+        private async void BtnForm1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
