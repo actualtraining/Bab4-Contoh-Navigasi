61b9409e6f9979cd858cbcbd1a25b37499dc33f1
erick <erick@DESKTOP-JIKBK0G>
2017-03-22 12:58:12 +0700

    menambahkan actionsheet

--- Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/SampleAlertPage.xaml.cs
+++ Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/SampleAlertPage.xaml.cs
@@ -15,7 +15,31 @@ namespace Bab4_Contoh_Navigasi
         public SampleAlertPage()
         {
             InitializeComponent();
+
+            btnAlertYesNo.Clicked += BtnAlertYesNo_Clicked;
+            btnAlert.Clicked += BtnAlert_Clicked;
         }
 
+        private async void BtnAlert_Clicked(object sender, EventArgs e)
+        {
+            var action = await DisplayActionSheet("Bahasa pemrograman favorite ?", "Cancel", null,
+                "C#", "Java", "VB", "Swift", "Phyton", "Go");
+
+            await DisplayAlert("Keterangan", "Anda memilih " + action,"OK");
+        }
+
+        private async void BtnAlertYesNo_Clicked(object sender, EventArgs e)
+        {
+            var answer = await DisplayAlert("Konfirmasi", "Apakah anda yakin untuk mendelete data?",
+                "Yes", "No");
+            if (answer)
+            {
+                await DisplayAlert("Keterangan", "Data berhasil di delete !", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Keterangan", "Data gagal untuk di delete !", "OK");
+            }
+        }
     }
 }
