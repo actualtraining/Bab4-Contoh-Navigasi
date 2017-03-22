c30eeb0c80548741e11936f973545baa190f61c6
erick <erick@DESKTOP-JIKBK0G>
2017-03-22 14:22:02 +0700

    memuat cangkang master page

---
+++ Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/PEBMasterPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bab4_Contoh_Navigasi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PEBMasterPage : MasterDetailPage
+    {
+        public PEBMasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
