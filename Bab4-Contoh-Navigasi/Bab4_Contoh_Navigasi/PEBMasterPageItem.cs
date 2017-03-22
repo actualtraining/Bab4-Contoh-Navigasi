4e28425178f25df7cf9eb55cd5c11a8c8b514908
erick <erick@DESKTOP-JIKBK0G>
2017-03-22 14:40:17 +0700

    menambahkan menu

---
+++ Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/PEBMasterPageItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bab4_Contoh_Navigasi
+{
+    public class PEBMasterPageItem
+    {
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetType { get; set; }
+    }
+
+
+}
