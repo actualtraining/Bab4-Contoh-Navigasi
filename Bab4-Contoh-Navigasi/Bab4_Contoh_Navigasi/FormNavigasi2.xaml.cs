52b4a3b9558f94d34248d3bf3f356788a3b63437
erick <erick@DESKTOP-JIKBK0G>
2017-03-22 13:07:48 +0700

    parameter dengan constructor

--- Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/FormNavigasi2.xaml.cs
+++ Bab4-Contoh-Navigasi/Bab4_Contoh_Navigasi/FormNavigasi2.xaml.cs
@@ -18,11 +18,25 @@ namespace Bab4_Contoh_Navigasi
             btnForm1.Clicked += BtnForm1_Clicked;
         }
 
-       
+        private string param;
+        public FormNavigasi2(string param)
+        {
+            InitializeComponent();
+            this.param = param;
+            btnForm1.Clicked += BtnForm1_Clicked;
+            btnParam.Clicked += BtnParam_Clicked;
+        }
+
+        private async void BtnParam_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Keterangan", "Param : " + param, "OK");
+        }
 
         private async void BtnForm1_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
         }
+
+
     }
 }
